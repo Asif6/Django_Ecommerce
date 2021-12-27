8cb8d4387272467271ae42fbc00c7a9b801bbd61
Asif6 <asiffoysal3@gmail.com>
2021-12-27 10:15:39 +0600

    Sort all product by category

--- Wishshop/Shop/views/index.py
+++ Wishshop/Shop/views/index.py
@@ -2,6 +2,7 @@ from django import views
 from django.shortcuts import render
 from django.views import View
 from Shop.models.category import Category
+from Shop.models.products import Product
 
 
 
@@ -11,7 +12,15 @@ from Shop.models.category import Category
 class Index(views.View):
     def get(self,request):
 
+        category_id=request.GET.get("category")
+        print("CATEGORYYYYYYYYYYYYYYYYYYYYYYYYYYY", category_id)
         category=Category.objects.all()
+
+        if category_id:
+            products=Product.objects.filter(sub_category_id=category_id)
+        else:
+            products=Product.objects.all().order_by("-id")
         data={}
         data['category']=category
+        data['product']=products
         return render(request,"index.html",data)
