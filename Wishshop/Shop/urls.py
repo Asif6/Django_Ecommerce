89c628707ee72986570fe23b1f39303eb56425dc
Asif6 <asiffoysal3@gmail.com>
2022-01-05 02:42:09 +0600

    Add custome templates for Reset Password System

--- Wishshop/Shop/urls.py
+++ Wishshop/Shop/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('login/',Login.as_view(),name='login'),
     path('logout/',Logout.as_view(),name='logout'),
 
-    path('password_reset/',auth_view.PasswordResetView.as_view(), name="password_reset"),
-    path('password_reset_done/',auth_view.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('password_resets_confirm/<uidb64>/<token>',auth_view.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('password_reset/',auth_view.PasswordResetView.as_view(template_name='user/password_reset.html'), name="password_reset"),
+    path('password_reset_done/',auth_view.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'),name='password_reset_done'),
+    path('password_resets_confirm/<uidb64>/<token>',auth_view.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'),name='password_reset_complete'),
 ]
